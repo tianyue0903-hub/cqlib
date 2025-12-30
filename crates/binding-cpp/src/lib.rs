cea0c4fb3c638145fcdda8002363f3b73064547a
健健 <jianjian001@outlook.com>
2025-12-30 16:07:45 +0800

    refactor: remove duplicate gitignore files and fix logic duplication in binding-cpp

--- crates/binding-cpp/src/lib.rs
+++ crates/binding-cpp/src/lib.rs
@@ -1,3 +1,5 @@
+use cqlib_core::add;
+
 #[cxx::bridge(namespace = "cqlib")]
 mod ffi {
     // 暴露给 C++ 调用的 Rust 函数列表
@@ -7,17 +9,8 @@ mod ffi {
     }
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[test]
+fn it_works() {
+    let result = add(2, 2);
+    assert_eq!(result, 4);
 }
